2e21e5822052bba2e7a007d94c2b980a4e6fe60b
arunkumar645 <arunkumarterala@gmail.com>
2025-01-29 09:49:57 +0530

    Method overloading

---
+++ Core-Java/Demo054.java
@@ -0,0 +1,22 @@
+class One {
+    public void method1() {
+        System.out.println("This is method1.and NO parameters");
+    }
+
+    public void method1(String message) {
+        System.out.println("Method Overloading 1 - With message: " + message);
+    }
+
+    public void method1(int number) {
+        System.out.println("Method Overloading 1 - With number: " + number);
+    }
+}
+
+public class Demo054 {
+    public static void main(String[] args) {
+        One obj = new One();
+        obj.method1();
+        obj.method1("Hello!");
+        obj.method1(42);
+    }
+}
